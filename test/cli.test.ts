d1cc6ddeb204b3e2f6f0259391b2ebabf7127d79
Kazushi Konosu <kazushi@konosu.me>
2024-08-17 18:32:33 +0900

    feat: init test under test/

---
+++ test/cli.test.ts
@@ -0,0 +1,8 @@
+import assert from 'node:assert/strict';
+import { describe, it } from 'node:test';
+
+describe('cli', () => {
+  it('should say hello', () => {
+    assert.equal('hello', 'hello');
+  });
+});
