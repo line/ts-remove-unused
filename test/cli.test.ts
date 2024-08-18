f0265cab176320943182dca77bd87cc64124ff30
Kazushi Konosu <kazushi@konosu.me>
2024-08-18 16:12:22 +0900

    feat: add basic logic to remove export keyword

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -150,4 +150,73 @@ describe('cli', () => {
       ],
     );
   });
+
+  it('should remove the export keyword', () => {
+    const files: { [name: string]: string } = {
+      'main.ts': `import { add } from './util/operations.js';
+          export const main = () => {};
+        `,
+      'util/operations.ts': `export const add = (a: number, b: number) => a + b;
+          export const subtract = (a: number, b: number) => a - b;
+          const multiply = (a: number, b: number) => a * b;
+          export const divide = (a: number, b: number) => a / b;
+          `,
+    };
+
+    const service = ts.createLanguageService({
+      getCompilationSettings() {
+        return {};
+      },
+      getScriptFileNames() {
+        return Object.keys(files);
+      },
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      getScriptVersion(_fileName) {
+        return '';
+      },
+      getScriptSnapshot(fileName) {
+        return ts.ScriptSnapshot.fromString(files[fileName] || '');
+      },
+      getCurrentDirectory: () => '.',
+
+      getDefaultLibFileName(options) {
+        return ts.getDefaultLibFileName(options);
+      },
+      fileExists: (name) => !!files[name],
+      readFile: (name) => files[name],
+      writeFile: (name, text) => {
+        files[name] = text;
+      },
+    });
+
+    const list = getExportsForService(service);
+
+    for (const item of list) {
+      if (item.count >= 2) {
+        continue;
+      }
+
+      const text = files[item.file];
+
+      if (!text) {
+        throw new Error('file not found');
+      }
+
+      const exportKeyword = findFirstNodeOfKind(
+        item.node,
+        ts.SyntaxKind.ExportKeyword,
+      );
+
+      if (!exportKeyword) {
+        throw new Error('export keyword not found');
+      }
+
+      const start = exportKeyword.getStart();
+      const end = exportKeyword.getEnd();
+
+      const replaced = `${text.slice(0, start)}${text.slice(end)}`;
+
+      console.log({ target: item.node.getText(), replaced });
+    }
+  });
 });
