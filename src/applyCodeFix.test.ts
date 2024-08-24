ff86153c8a6899eabf48f12a70ecc7bc70255bf3
Kazushi Konosu <kazushi@konosu.me>
2024-08-25 00:43:20 +0900

    refactor: move test to applyCodeFix.test.ts

--- src/applyCodeFix.test.ts
+++ src/applyCodeFix.test.ts
@@ -1,59 +1,59 @@
-import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
+import { FileService } from './FileService.js';
 import ts from 'typescript';
-import { FileService } from '../src/FileService.js';
+import assert from 'node:assert/strict';
 import {
   applyCodeFix,
   fixIdDelete,
   fixIdDeleteImports,
-} from '../src/applyCodeFix.js';
+} from './applyCodeFix.js';
 
-describe('cli', () => {
-  const setup = () => {
-    const fileService = new FileService();
+const setup = () => {
+  const fileService = new FileService();
 
-    const languageService = ts.createLanguageService({
-      getCompilationSettings() {
-        return {};
-      },
-      getScriptFileNames() {
-        return fileService.getFileNames();
-      },
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      getScriptVersion(fileName) {
-        return fileService.getVersion(fileName);
-      },
-      getScriptSnapshot(fileName) {
-        return ts.ScriptSnapshot.fromString(fileService.get(fileName));
-      },
-      getCurrentDirectory: () => '.',
+  const languageService = ts.createLanguageService({
+    getCompilationSettings() {
+      return {};
+    },
+    getScriptFileNames() {
+      return fileService.getFileNames();
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    getScriptVersion(fileName) {
+      return fileService.getVersion(fileName);
+    },
+    getScriptSnapshot(fileName) {
+      return ts.ScriptSnapshot.fromString(fileService.get(fileName));
+    },
+    getCurrentDirectory: () => '.',
 
-      getDefaultLibFileName(options) {
-        return ts.getDefaultLibFileName(options);
-      },
-      fileExists: (name) => fileService.exists(name),
-      readFile: (name) => fileService.get(name),
-    });
+    getDefaultLibFileName(options) {
+      return ts.getDefaultLibFileName(options);
+    },
+    fileExists: (name) => fileService.exists(name),
+    readFile: (name) => fileService.get(name),
+  });
 
-    return { languageService, fileService };
-  };
+  return { languageService, fileService };
+};
 
+describe('applyCodeFix', () => {
   it('should clean up unused identifiers', () => {
     const { languageService, fileService } = setup();
     fileService.set(
       'main.ts',
       `import { add } from './util/operations.js';
-              const main = () => {};
-            `,
+                const main = () => {};
+              `,
     );
 
     fileService.set(
       'util/operations.ts',
       `export const add = (a: number, b: number) => a + b;
-            const subtract = (a: number, b: number) => a - b;
-            const multiply = (a: number, b: number) => a * b;
-            const divide = (a: number, b: number) => a / b;
-              `,
+              const subtract = (a: number, b: number) => a - b;
+              const multiply = (a: number, b: number) => a * b;
+              const divide = (a: number, b: number) => a / b;
+                `,
     );
 
     applyCodeFix({
@@ -74,14 +74,14 @@ describe('cli', () => {
     fileService.set(
       'main.ts',
       `import { add } from './util/operations.js';
-        `,
+          `,
     );
 
     fileService.set(
       'util/operations.ts',
       `import { readFileSync } from 'node:fs';
-        export const add = (a: number, b: number) => a + b;
-`,
+          export const add = (a: number, b: number) => a + b;
+  `,
     );
 
     applyCodeFix({
