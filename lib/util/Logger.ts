9ce74e44161d89c2327a395a868ddcad7b0d78d1
Kazushi Konosu <kazushi@konosu.me>
2024-09-11 23:50:31 +0900

    fix: handle execution in non tty

--- lib/util/Logger.ts
+++ lib/util/Logger.ts
@@ -1,6 +1,11 @@
-export interface Logger {
-  write(text: string): void;
-  clearLine(dir: -1 | 0 | 1): void;
-  cursorTo(x: number): void;
-  isTTY: boolean;
-}
+export type Logger =
+  | {
+      write(text: string): void;
+      clearLine(dir: -1 | 0 | 1): void;
+      cursorTo(x: number): void;
+      isTTY: true;
+    }
+  | {
+      write(text: string): void;
+      isTTY: false;
+    };
