d8ccc4d1740e98ae7d3b88ef0f000f45baeb98d6
Kazushi Konosu <kazushi@konosu.me>
2024-09-15 22:23:13 +0900

    test: update test to prevent issue where export and file is reported for the same file

--- test/execute.test.ts
+++ test/execute.test.ts
@@ -35,7 +35,10 @@ describe('cli', () => {
       },
     });
 
-    assert.equal(!!output.find((line) => line.includes('a.ts')), true);
-    assert.equal(!!output.find((line) => line.includes('b.ts')), true);
+    assert.equal(!!output.find((line) => line.includes('project/a.ts')), true);
+    assert.equal(
+      output.filter((line) => line.includes('project/b.ts')).length,
+      1,
+    );
   });
 });
