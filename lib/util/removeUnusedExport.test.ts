3a78f8ccb1f6c4b5064d84c21d345058ae138d7a
Kazushi Konosu <kazushi@konosu.me>
2024-09-23 21:46:57 +0900

    test: add test case for named imports via whole re-exports

--- lib/util/removeUnusedExport.test.ts
+++ lib/util/removeUnusedExport.test.ts
@@ -791,6 +791,25 @@ a_namespace.a;`,
       );
       assert.equal(fileService.get('/app/a.ts'), `export const a = 'a';`);
     });
+
+    it('should not remove declaration that is used named imported in some other file via a whole-reexport', () => {
+      const { languageService, fileService } = setup();
+
+      fileService.set('/app/main.ts', `import { a } from './a_reexport';`);
+      fileService.set('/app/a_reexport.ts', `export * from './a';`);
+      fileService.set('/app/a.ts', `export const a = 'a';`);
+
+      removeUnusedExport({
+        languageService,
+        fileService,
+        targetFile: ['/app/a.ts', '/app/a_reexport.ts'],
+      });
+      assert.equal(
+        fileService.get('/app/a_reexport.ts'),
+        `export * from './a';`,
+      );
+      assert.equal(fileService.get('/app/a.ts'), `export const a = 'a';`);
+    });
   });
 
   describe('locally used declaration but not used in any other file', () => {
