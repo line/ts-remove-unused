f225cd2d5e5e2766ff2c50d15a9963e1b12820ec
Kazushi Konosu <konosu.kazushi@linecorp.com>
2024-10-10 16:51:59 +0900

    feat: implement basic logic

--- lib/util/collectImports.ts
+++ lib/util/collectImports.ts
@@ -1,6 +1,7 @@
 import ts from 'typescript';
 import { Graph } from './Graph.js';
 import { FileService } from './FileService.js';
+import { getFileFromModuleSpecifierText } from './getFileFromModuleSpecifierText.js';
 
 export const collectImports = ({
   fileService,
@@ -10,9 +11,37 @@ export const collectImports = ({
   program: ts.Program;
 }) => {
   const graph = new Graph();
+  const files = fileService.getFileNames();
 
-  // fixme
-  console.log(Object.keys({ fileService, program }));
+  for (const file of files) {
+    const sourceFile = program.getSourceFile(file);
+    if (!sourceFile) {
+      continue;
+    }
 
+    const visit = (node: ts.Node) => {
+      if (
+        ts.isImportDeclaration(node) &&
+        ts.isStringLiteral(node.moduleSpecifier)
+      ) {
+        const file = getFileFromModuleSpecifierText({
+          specifier: node.moduleSpecifier.text,
+          program,
+          fileName: sourceFile.fileName,
+          fileService,
+        });
+
+        if (file) {
+          graph.addEdge(sourceFile.fileName, file);
+        }
+
+        return;
+      }
+
+      node.forEachChild(visit);
+    };
+
+    sourceFile.forEachChild(visit);
+  }
   return graph;
 };
