7a71df9739142d84634b8c9da135bae45c87ea04
Kazushi Konosu <konosu.kazushi@linecorp.com>
2024-10-17 15:56:30 +0900

    chore: enable no-shadow

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,10 @@ export default tseslint.config(
   { ignores: ['dist'] },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-shadow': 'error',
+    },
+  },
   eslintPluginPrettierRecommended,
 );
