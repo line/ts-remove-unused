25ae21776fee8758bb3b1c8f10aced78cb8d742e
Kazushi Konosu <kazushi@konosu.me>
2024-10-19 18:12:23 +0900

    test: add test case for priority

--- lib/util/TaskManager.test.ts
+++ lib/util/TaskManager.test.ts
@@ -2,8 +2,6 @@ import { describe, it } from 'node:test';
 import { TaskManager } from './TaskManager.js';
 import assert from 'node:assert/strict';
 
-// const wait = (ms: number) => new Promise<void>((r) => setTimeout(r, ms));
-
 describe('TaskManager', () => {
   it('should execute tasks', async () => {
     let result = '';
@@ -21,14 +19,39 @@ describe('TaskManager', () => {
     let result = '';
 
     const taskManager = new TaskManager(async (c) => {
+      result += `${c.file}\n`;
+
       if (c.file === 'a') {
         c.add('c');
       }
-      result += `${c.file}\n`;
     });
 
-    taskManager.execute(['a', 'b']);
+    await taskManager.execute(['a', 'b']);
 
     assert.equal(result, 'a\nb\nc\n');
+  });
+
+  it('should prioritize new tasks over existing tasks', async () => {
+    let result = '';
+    const count = { a: 0, b: 0, c: 0 };
+
+    const taskManager = new TaskManager(async (c) => {
+      count[c.file as 'a' | 'b' | 'c']++;
+      await Promise.resolve();
+
+      if (c.signal.aborted) {
+        return;
+      }
+
+      result += `${c.file}:${count[c.file as 'a' | 'b' | 'c']}\n`;
+
+      if (c.file === 'a') {
+        c.add('b');
+      }
+    });
+
+    await taskManager.execute(['a', 'b', 'c']);
+
+    assert.equal(result, 'a:1\nc:1\nb:2\n');
   });
 });
