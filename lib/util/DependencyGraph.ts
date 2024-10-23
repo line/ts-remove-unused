f31a02b9a8c5ed03b9ea086bc7061c27a872d811
Kazushi Konosu <kazushi@konosu.me>
2024-10-23 22:35:50 +0900

    fix: delete whole reexpoort statements if the file has been deleted

--- lib/util/DependencyGraph.ts
+++ lib/util/DependencyGraph.ts
@@ -4,12 +4,16 @@ export class DependencyGraph extends Graph<{
   depth: number;
   hasReexport: boolean;
   fromDynamic: Set<string>;
+  wholeReexportSpecifier: Map<string, string>;
 }> {
   constructor() {
     super(() => ({
       depth: 0,
       hasReexport: false,
       fromDynamic: new Set<string>(),
+      // will not be updated when we delete a file,
+      // but it's fine since we only use it to find the used specifier from a given file path.
+      wholeReexportSpecifier: new Map<string, string>(),
     }));
   }
 
