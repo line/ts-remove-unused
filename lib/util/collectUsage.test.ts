b22ba6328d79ab40c021075b459a9eba9526fc8d
Kazushi Konosu <kazushi@konosu.me>
2024-11-04 19:33:18 +0900

    test: add test case for multiple named imports

--- lib/util/collectUsage.test.ts
+++ lib/util/collectUsage.test.ts
@@ -3,7 +3,7 @@ import assert from 'node:assert/strict';
 import { collectUsage } from './collectUsage.js';
 
 describe('collectUsage', () => {
-  it('should collect basic usage', () => {
+  it('should collect a single named import', () => {
     const result = collectUsage({
       file: '/app/a.ts',
       content: 'import { b } from "./b";',
@@ -12,6 +12,18 @@ describe('collectUsage', () => {
 
     assert.deepEqual(result, {
       '/app/b.ts': ['b'],
+    });
+  });
+
+  it('should collect multiple named imports', () => {
+    const result = collectUsage({
+      file: '/app/a.ts',
+      content: 'import { b, c } from "./b";',
+      destFiles: new Set(['/app/b.ts']),
+    });
+
+    assert.deepEqual(result, {
+      '/app/b.ts': ['b', 'c'],
     });
   });
 });
