2ae12b0f122be2313f2c797976a5cf3869fbe29b
Kazushi Konosu <kazushi@konosu.me>
2024-11-05 00:42:24 +0900

    test: add test for multiple import declarations referencing the same file

--- lib/util/collectUsage.test.ts
+++ lib/util/collectUsage.test.ts
@@ -87,4 +87,16 @@ describe('collectUsage', () => {
       '/app/c.ts': new Set(['c']),
     });
   });
+
+  it('should collect when there are multiple import declarations referencing the same file', () => {
+    const result = collectUsage({
+      file: '/app/a.ts',
+      content: 'import { b } from "./b"; import { c } from "./b";',
+      destFiles: new Set(['/app/b.ts']),
+    });
+
+    assert.deepEqual(result, {
+      '/app/b.ts': new Set(['b', 'c']),
+    });
+  });
 });
