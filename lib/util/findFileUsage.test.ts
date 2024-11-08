7bea8c1488c7d2123a2e7bdd9059dbf617705248
Kazushi Konosu <kazushi@konosu.me>
2024-11-08 16:46:06 +0900

    test: add basic test case for findFileUsage

---
+++ lib/util/findFileUsage.test.ts
@@ -0,0 +1,38 @@
+import { describe, it } from 'node:test';
+import { MemoryFileService } from './MemoryFileService.js';
+import { createProgram } from './createProgram.js';
+import { collectImports } from './collectImports.js';
+import { findFileUsage } from './findFileUsage.js';
+import assert from 'node:assert/strict';
+
+describe('findFileUsage', () => {
+  it('should return a list of identifiers that are used', () => {
+    const fileService = new MemoryFileService();
+    fileService.set('/app/main.ts', `import { a, a2 } from './a';`);
+    fileService.set(
+      '/app/a.ts',
+      `export const a = 'a'; export const a2 = 'a2';`,
+    );
+
+    const program = createProgram({
+      fileService,
+      options: {},
+      projectRoot: '/app',
+    });
+
+    const graph = collectImports({
+      fileService,
+      program,
+      entrypoints: ['/app/main.ts'],
+    });
+
+    const result = findFileUsage({
+      targetFile: '/app/a.ts',
+      vertexes: graph.eject(),
+      files: fileService.eject(),
+      options: {},
+    });
+
+    assert.deepEqual(result, ['a', 'a2']);
+  });
+});
