d36319dd6d2a38d2eacaf12bd21577ca45a0c5db
Kazushi Konosu <kazushi@konosu.me>
2024-11-08 16:57:18 +0900

    test: add test case

--- lib/util/findFileUsage.test.ts
+++ lib/util/findFileUsage.test.ts
@@ -35,4 +35,36 @@ describe('findFileUsage', () => {
 
     assert.deepEqual(result, new Set(['a2', 'a']));
   });
+
+  it('should return a list of identifiers only including items that are exported from the target file', () => {
+    const fileService = new MemoryFileService();
+    fileService.set(
+      '/app/main.ts',
+      `import { a, a2 } from './a';import { b } from './b';`,
+    );
+    fileService.set(
+      '/app/a.ts',
+      `export const a = 'a'; export const a2 = 'a2';`,
+    );
+    fileService.set('/app/b.ts', `export const b = 'b';`);
+
+    const program = createProgram({
+      fileService,
+      options: {},
+      projectRoot: '/app',
+    });
+    const graph = collectImports({
+      fileService,
+      program,
+      entrypoints: ['/app/main.ts'],
+    });
+    const result = findFileUsage({
+      targetFile: '/app/a.ts',
+      vertexes: graph.eject(),
+      files: fileService.eject(),
+      options: {},
+    });
+
+    assert.deepEqual(result, new Set(['a2', 'a']));
+  });
 });
