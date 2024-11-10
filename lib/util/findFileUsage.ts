ed88f19ca236f894ad5eb0d5ed93b514c2357caf
Kazushi Konosu <kazushi@konosu.me>
2024-11-11 01:53:03 +0900

    fix: filter usage to only include exports from the given file

--- lib/util/findFileUsage.ts
+++ lib/util/findFileUsage.ts
@@ -11,7 +11,59 @@ const fallback = () => ({
   },
 });
 
-const getUsed = ({
+const ALL_EXPORTS_OF_UNKNOWN_FILE = '__all_exports_of_unknown_file__';
+
+const getExportsOfFile = ({
+  targetFile,
+  vertexes,
+  files,
+  options,
+}: {
+  targetFile: string;
+  vertexes: Vertexes;
+  files: Map<string, string>;
+  options: ts.CompilerOptions;
+}) => {
+  const result: string[] = [];
+
+  const stack = [targetFile];
+
+  while (stack.length) {
+    const item = stack.pop();
+
+    if (!item) {
+      break;
+    }
+
+    const vertex = vertexes.get(item) || fallback();
+
+    const { exports } = parseFile({
+      file: item,
+      content: files.get(item) || '',
+      destFiles: new Set(vertex.to),
+      options,
+    });
+
+    exports.forEach((it) => {
+      if (it.kind === ts.SyntaxKind.ExportDeclaration && it.type === 'whole') {
+        if (it.file) {
+          stack.push(it.file);
+        } else {
+          // unknown file
+          result.push(ALL_EXPORTS_OF_UNKNOWN_FILE);
+        }
+
+        return;
+      }
+
+      result.push(...(Array.isArray(it.name) ? it.name : [it.name]));
+    });
+  }
+
+  return new Set(result);
+};
+
+export const findFileUsage = ({
   targetFile,
   options,
   vertexes,
@@ -23,10 +75,19 @@ const getUsed = ({
   options: ts.CompilerOptions;
 }) => {
   const result: string[] = [];
+  const exportsOfTargetFile = getExportsOfFile({
+    targetFile,
+    vertexes,
+    files,
+    options,
+  });
   const stack: { file: string; to: string }[] = [];
-  vertexes
-    .get(targetFile)
-    ?.from.forEach((file) => stack.push({ file, to: targetFile }));
+  vertexes.get(targetFile)?.from.forEach((file) =>
+    stack.push({
+      file,
+      to: targetFile,
+    }),
+  );
 
   while (stack.length) {
     const item = stack.pop()!;
@@ -63,7 +124,10 @@ const getUsed = ({
         }
 
         vertexes.get(it.file)?.from.forEach((f) => {
-          stack.push({ file: f, to: it.file });
+          stack.push({
+            file: f,
+            to: it.file,
+          });
         });
         return;
       }
@@ -75,20 +139,7 @@ const getUsed = ({
     });
   }
 
-  return result;
-};
-
-export const findFileUsage = ({
-  targetFile,
-  options,
-  vertexes,
-  files,
-}: {
-  targetFile: string;
-  vertexes: Vertexes;
-  files: Map<string, string>;
-  options: ts.CompilerOptions;
-}) => {
-  const used = getUsed({ targetFile, options, vertexes, files });
-  return new Set(used);
+  return new Set(
+    result.filter((it) => exportsOfTargetFile.has(it) || it === '*'),
+  );
 };
