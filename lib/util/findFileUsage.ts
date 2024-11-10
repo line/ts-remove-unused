bd250e16cf3603511c8a71eb913c101acd7f8949
Kazushi Konosu <kazushi@konosu.me>
2024-11-11 00:32:02 +0900

    refactor: rewrite findFileUsage with stack

--- lib/util/findFileUsage.ts
+++ lib/util/findFileUsage.ts
@@ -11,6 +11,73 @@ const fallback = () => ({
   },
 });
 
+const getUsed = ({
+  targetFile,
+  options,
+  vertexes,
+  files,
+}: {
+  targetFile: string;
+  vertexes: Vertexes;
+  files: Map<string, string>;
+  options: ts.CompilerOptions;
+}) => {
+  const result: string[] = [];
+  const stack: { file: string; to: string }[] = [];
+  vertexes
+    .get(targetFile)
+    ?.from.forEach((file) => stack.push({ file, to: targetFile }));
+
+  while (stack.length) {
+    const item = stack.pop()!;
+
+    if (!item) {
+      break;
+    }
+
+    const { file, to } = item;
+
+    const vertex = vertexes.get(file) || fallback();
+
+    const { imports } = parseFile({
+      file,
+      content: files.get(file) || '',
+      destFiles: new Set(vertex.to),
+      options,
+    });
+
+    const list = imports[to] || [];
+
+    list.forEach((it) => {
+      if (typeof it === 'object' && it.type === 'wholeReexport') {
+        const n = vertexes.get(it.file);
+
+        if (!n) {
+          return;
+        }
+
+        // is entrypoint
+        if (n.data.depth === 0) {
+          result.push('*');
+          return;
+        }
+
+        vertexes.get(it.file)?.from.forEach((f) => {
+          stack.push({ file: f, to: it.file });
+        });
+        return;
+      }
+
+      if (typeof it === 'string') {
+        result.push(it);
+        return;
+      }
+    });
+  }
+
+  return result;
+};
+
 export const findFileUsage = ({
   targetFile,
   options,
@@ -22,55 +89,6 @@ export const findFileUsage = ({
   files: Map<string, string>;
   options: ts.CompilerOptions;
 }) => {
-  const result = new Set<string>();
-
-  // vertex doesn't exist when
-  // - there are no imports in the entrypoint
-  // - the file is unreachable from the entrypoint and has no connections between other unreachable files
-  const vertex = vertexes.get(targetFile) || fallback();
-
-  for (const fromFile of vertex.from) {
-    const v = vertexes.get(fromFile) || fallback();
-
-    const collected = parseFile({
-      file: fromFile,
-      content: files.get(fromFile) || '',
-      destFiles: new Set(v.to),
-      options,
-    });
-
-    const list = collected.imports[targetFile];
-
-    if (!list) {
-      continue;
-    }
-
-    for (const item of list) {
-      if (typeof item === 'string') {
-        result.add(item);
-        continue;
-      }
-
-      const n = vertexes.get(item.file);
-
-      if (!n) {
-        continue;
-      }
-
-      // is entrypoint
-      if (n.data.depth === 0) {
-        result.add('*');
-        continue;
-      }
-
-      findFileUsage({
-        targetFile: item.file,
-        vertexes,
-        files,
-        options,
-      }).forEach((it) => result.add(it));
-    }
-  }
-
-  return result;
+  const used = getUsed({ targetFile, options, vertexes, files });
+  return new Set(used);
 };
